2d373880227218740fd5be70b7d8253cef684805
Vlad Rudenua <rudenua.dev@gmail.com>
2022-12-27 16:15:36 +0300

    fix(project-name): rename project from dart_cli_tool => cli_tool

--- bin/dart_cli_tool.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_cli_tool/dart_cli_tool.dart' as dart_cli_tool;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_cli_tool.calculate()}!');
-}
--- test/dart_cli_tool_test.dart
+++ test/dart_cli_tool_test.dart
@@ -1,8 +1,1 @@
-import 'package:dart_cli_tool/dart_cli_tool.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
